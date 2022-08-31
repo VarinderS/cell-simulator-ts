8eda05635861daf4e8f398770a8aaf48f1b3ebcb
Varinder Singh <mail@varinder.nz>
2022-09-01 08:05:47 +1200

    Introduced ability to toggle cell on or off, and started using convention of rowIndex and columnIndex instead of x/y coordinates

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -8,7 +8,7 @@ interface GridProps {
 }
 
 function Grid({ gridColumns = 6, gridRows = 6, debug = true }: GridProps) {
-  const [grid] = React.useState<boolean[][]>([
+  const [grid, setGrid] = React.useState<boolean[][]>([
     [false, true, false, false, false, false],
     [false, false, false, false, false, false],
     [false, false, false, false, false, false],
@@ -17,31 +17,47 @@ function Grid({ gridColumns = 6, gridRows = 6, debug = true }: GridProps) {
     [false, false, false, false, false, false],
   ]);
 
+  function toggleCellAtLocation({
+    rowIndex,
+    columnIndex,
+  }: {
+    rowIndex: number;
+    columnIndex: number;
+  }) {
+    const gridCopy = [...grid];
+    const currentCellState = grid[rowIndex][columnIndex];
+    const toggledCellState = currentCellState ? false : true;
+    gridCopy[rowIndex][columnIndex] = toggledCellState;
+    setGrid(gridCopy);
+  }
+
   return (
     <div className="flex flex-col gap-0.5">
       {grid.map((gridCells, rowIndex) => {
         const rowName = `row-${rowIndex}`;
         return (
           <div key={rowName} className="flex gap-0.5">
-            {gridCells.map((isAlive, cellIndex) => {
-              const cellName = `cell-${cellIndex}`;
-              const xAxisValue = cellIndex;
-              const yAxisValue = rowIndex;
-              const testid = `${xAxisValue}-${yAxisValue}`;
+            {gridCells.map((cell, columnIndex) => {
+              const cellName = `cell-${columnIndex}`;
+              const testid = `${rowIndex}-${columnIndex}`;
               return (
                 <button
                   key={cellName}
                   data-testid={testid}
                   className={classNames(
+                    "transition",
                     "flex items-center justify-center",
                     "w-12 h-12",
                     "bg-gray-100",
-                    { "bg-blue-500 text-white": isAlive },
+                    { "bg-blue-500 text-white": cell },
                   )}
+                  onClick={() =>
+                    toggleCellAtLocation({ rowIndex, columnIndex })
+                  }
                 >
                   {debug && (
                     <span>
-                      {xAxisValue} {yAxisValue}
+                      {columnIndex} {rowIndex}
                     </span>
                   )}
                 </button>
