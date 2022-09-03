051285f01750c5f2237f84690c986fb2a8e8d139
Varinder Singh <mail@varinder.nz>
2022-09-04 10:57:44 +1200

    Moved initial grid to a constant, this is required for the reset button functionality

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -82,15 +82,18 @@ function createNextGeneration(grid: boolean[][]) {
   return gridCopy;
 }
 
+
+const INITIAL_GRID = [
+  [false, false, false, false, false, false],
+  [false, false, true, false, false, false],
+  [false, false, false, true, false, false],
+  [false, true, true, true, false, false],
+  [false, false, false, false, false, false],
+  [false, false, false, false, false, false],
+];
+
 function Grid({ gridColumns = 6, gridRows = 6, debug = true }: GridProps) {
-  const [grid, setGrid] = React.useState<boolean[][]>([
-    [false, true, false, false, false, false],
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false],
-  ]);
+  const [grid, setGrid] = React.useState<boolean[][]>(INITIAL_GRID);
 
   function toggleCellAtLocation({
     rowIndex,
