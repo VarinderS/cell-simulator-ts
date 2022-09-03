613dbab4d3d4cd3ba99752599fdbe87cdeb406b0
Varinder Singh <mail@varinder.nz>
2022-09-04 10:57:02 +1200

    Introduced createNextGeneration function that takes current generation grid and returns a new grid cells toggled on or off

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -11,6 +11,77 @@ function copyGrid(grid: boolean[][]) {
   return JSON.parse(JSON.stringify(grid));
 }
 
+function createNextGeneration(grid: boolean[][]) {
+  const gridCopy = copyGrid(grid);
+
+  const totalRows = grid.length;
+
+  for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+    const row = grid[rowIndex];
+    const totalCells = row.length;
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      const isCellAlive = grid[rowIndex][columnIndex];
+
+      const northernRowIndex = (rowIndex - 1) % totalRows;
+      const northernRow = grid.at(northernRowIndex);
+
+      const centralRow = row;
+
+      const southernRowIndex = (rowIndex + 1) % totalRows;
+      const southernRow = grid.at(southernRowIndex);
+
+      const northernNeighbor = northernRow?.at(columnIndex);
+
+      const northEasternColumnIndex = (columnIndex + 1) % totalCells;
+      const northEasternNeighbor = northernRow?.at(northEasternColumnIndex);
+
+      const easternColumnIndex = (columnIndex + 1) % totalCells;
+      const easternNeighbor = centralRow.at(easternColumnIndex);
+
+      const southEasternColumnIndex = (columnIndex + 1) % totalCells;
+      const southEasternNeighbor = southernRow?.at(southEasternColumnIndex);
+
+      const southernNeighbor = southernRow?.at(columnIndex);
+
+      const southWesternColumnIndex = (columnIndex - 1) % totalCells;
+      const southWesternNeighbor = southernRow?.at(southWesternColumnIndex);
+
+      const westernColumnIndex = (columnIndex - 1) % totalCells;
+      const westernNeighbor = centralRow?.at(westernColumnIndex);
+
+      const northWesternColumnIndex = (columnIndex - 1) % totalCells;
+      const northWesternNeighbor = northernRow?.at(northWesternColumnIndex);
+
+      const totalAliveNeighbors = [
+        northernNeighbor,
+        northEasternNeighbor,
+        easternNeighbor,
+        southEasternNeighbor,
+        southernNeighbor,
+        southWesternNeighbor,
+        westernNeighbor,
+        northWesternNeighbor,
+      ].filter(Boolean).length;
+
+      if (isCellAlive) {
+        if (totalAliveNeighbors < 2) {
+          gridCopy[rowIndex][columnIndex] = false;
+        }
+
+        if (totalAliveNeighbors > 3) {
+          gridCopy[rowIndex][columnIndex] = false;
+        }
+      } else {
+        if (totalAliveNeighbors === 3) {
+          gridCopy[rowIndex][columnIndex] = true;
+        }
+      }
+    }
+  }
+
+  return gridCopy;
+}
+
 function Grid({ gridColumns = 6, gridRows = 6, debug = true }: GridProps) {
   const [grid, setGrid] = React.useState<boolean[][]>([
     [false, true, false, false, false, false],
